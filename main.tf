62ae5863b92c27b88c3eb28be1dfdeb19efd4608
venkatapavan99 <84392859+venkatapavan99@users.noreply.github.com>
2025-02-17 22:12:38 +0530

    Create main.tf
    Basic Terraform code

---
+++ main.tf
@@ -0,0 +1,9 @@
+variable "instance_type" {
+  description = "Instance type"
+  type = string
+  default = "t2.micro"
+}
+provider "aws" {
+  region = "us-east-1"
+  ami_id = "ami-0604f27d956d83a4d"
+}
